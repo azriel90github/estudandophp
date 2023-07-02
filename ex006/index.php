6e6541d3b7769c3e904e5c747eb38ba76613340f
Azriel@armando <114200974+azriel90github@users.noreply.github.com>
2023-07-02 22:46:40 +0100

    01

---
+++ ex006/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes</title>
+</head>
+<body>
+    <h1>Testes</h1>
+    <p>
+        <?php 
+            $nomeCompletoCliente = "Camel Case"; // Declaração Camel-Case
+            $telefone_contacto_fornecedor = "Fornecedor"; // Declaração  Snake-Case
+        ?>
+    </p>
+    
+</body>
+</html>
